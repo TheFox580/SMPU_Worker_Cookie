77e2265e6ea2333d361e28dffd768e3ae3580313
Fox <zoelliotmitong@gmail.com>
2023-12-13 00:34:40 +0100

    started mod

---
+++ src/main/java/net/thefox580/smpu_worker_cookie/SMPUnityWorkerCookie.java
@@ -0,0 +1,60 @@
+package net.thefox580.smpu_worker_cookie;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(SMPUnityWorkerCookie.MOD_ID)
+public class SMPUnityWorkerCookie {
+
+    public static final String MOD_ID = "smpu_worker_cookie";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public SMPUnityWorkerCookie() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+
+        modEventBus.addListener(this::addCreative);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+    }
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+        }
+    }
+}
---
+++ src/main/java/net/thefox580/smpu_worker_cookie/item/custom/ModFood.java
@@ -0,0 +1,14 @@
+package net.thefox580.smpu_worker_cookie.item.custom;
+
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
+
+public class ModFood {
+    public static final FoodProperties WORKER_COOKIE = new FoodProperties.Builder().nutrition(4)
+            .saturationMod(1.2F)
+            .effect(() -> new MobEffectInstance(MobEffects.ABSORPTION, 100, 1), 1f)
+            .effect(() -> new MobEffectInstance(MobEffects.REGENERATION, 100, 2), 1f)
+            .effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 600, 1), 1f).build();
+
+}
